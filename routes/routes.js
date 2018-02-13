55e74909e7ae528e3f77e3c763b1ac9957ac2957
Ondrej Komarek <komoi@seznam.cz>
2018-02-13 18:50:24 +0100

    Fix bad route

--- routes/routes.js
+++ routes/routes.js
@@ -2,10 +2,11 @@ var appRouter = function(app) {
 app.get("/", function(req, res) {
       res.send("Hello World");
   });
-}
 
 app.post("/local_time", function(req, res) {
       res.send("4:20 pm");
   });
 }
+
+
 module.exports = appRouter;
