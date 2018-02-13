c0fd182c7a18d6f5a5ae47aa4cad348b5cd0f0ee
Ondrej Komarek <komoi@seznam.cz>
2018-02-13 19:40:35 +0100

    Changes in response header

--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,10 @@ app.get("/", function(req, res) {
   });
 
 app.post("/local_time", function(req, res) {
-	  res.writeHead(200, {'Content-Type': 'application/json'});
+	  //res.setHeader("Content-Type", "application/json" );
+	 // res.writeHead(200);
+	 res.set('Content-Type', 'application/json');
+
       res.send("\"messages\": [\r\n  {\r\n    \"speech\": \"It is 4pm in there\",\r\n    \"type\": 0\r\n  },\r\n  {\r\n    \"speech\": \"It is 4pm in there\",\r\n    \"type\": 0\r\n  }\r\n]");
   });
 }
