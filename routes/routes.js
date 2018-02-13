0fcd843566160efa6992d4e780c94deaa29b172e
Ondrej Komarek <komoi@seznam.cz>
2018-02-13 19:58:43 +0100

    Added sample response vol2

--- routes/routes.js
+++ routes/routes.js
@@ -7,7 +7,12 @@ app.post("/local_time", function(req, res) {
 	  //res.setHeader("Content-Type", "application/json" );
 	 // res.writeHead(200);
 	 //res.set('Content-Type', 'application/json');
-	res.json({
+ response = "This is a sample response from your webhook!" //Default response from the webhook to show it's working
+
+
+  res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
+  res.send(JSON.stringify({ "speech": response, "displayText": response 
+	/*res.json({
 "messages": [
   {
     "speech": "It is 4pm in there",
@@ -19,8 +24,10 @@ app.post("/local_time", function(req, res) {
   }
 ]
 });
+*/
       //res.send("\"messages\": [\r\n  {\r\n    \"speech\": \"It is 4pm in there\",\r\n    \"type\": 0\r\n  },\r\n  {\r\n    \"speech\": \"It is 4pm in there\",\r\n    \"type\": 0\r\n  }\r\n]");
   });
+
 }
 
 
